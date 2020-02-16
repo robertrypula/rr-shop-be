89a5e295271f5c7544e1e6dea75776254ad868e1
Robert Rypuła <robert.rypula@gmail.com>
2020-02-16 22:49:32 +0100

    Implements categoryIds parameter in products endpoint

--- src/controllers/product-controller.ts
+++ src/controllers/product-controller.ts
@@ -5,12 +5,29 @@ import { SelectQueryBuilder } from 'typeorm/query-builder/SelectQueryBuilder';
 import { Category } from '../entity/category';
 import { Product } from '../entity/product';
 
+/*
+  Where in:
+    https://github.com/typeorm/typeorm/issues/1239
+    https://github.com/typeorm/typeorm/issues/2135
+*/
+
 export class ProductController {
   public constructor(protected repository: Repository<Product> = getRepository(Product)) {}
 
   public async all(req: Request, res: Response): Promise<void> {
     const isSimple: boolean = req.query.simple && req.query.simple === 'true';
-    const categoryId: number = req.query.categoryId && +req.query.categoryId > 0 ? req.query.categoryId : null;
+    const categoryIds: number[] =
+      typeof req.query.categoryIds !== 'undefined'
+        ? req.query.categoryIds === ''
+          ? []
+          : req.query.categoryIds.split(',').map((categoryId: string): number => +categoryId)
+        : null;
+
+    if (categoryIds && categoryIds.length === 0) {
+      res.send([]);
+      return;
+    }
+
     const queryBuilder: SelectQueryBuilder<Product> = this.repository
       .createQueryBuilder('product')
       .select([
@@ -18,14 +35,19 @@ export class ProductController {
         ...(isSimple ? [] : ['name', 'slug', 'price'].map(column => `product.${column}`)),
         'category.id'
       ])
-      .leftJoin('product.categories', 'category');
+      .leftJoin('product.categories', 'category', '');
     let products: Product[];
 
-    categoryId && queryBuilder.where('category.id = :categoryId', { categoryId });
+    if (categoryIds) {
+      queryBuilder.where('category.id IN (:...categoryIds)', { categoryIds });
+    }
+
     products = await queryBuilder.getMany();
     products.forEach((product: Product): void => {
-      product.categoryIds = product.categories.map((category: Category) => category.id);
-      delete product.categories;
+      if (product.categories) {
+        product.categoryIds = product.categories.map((category: Category) => category.id);
+        delete product.categories;
+      }
     });
 
     res.send(products);
--- src/migration/test.sql
+++ src/migration/test.sql
@@ -1,1 +1,8 @@
-select * from category;
+SELECT
+  "product"."id" AS "product_id", "category"."id" AS "category_id"
+FROM
+  "product" "product"
+LEFT JOIN "product_categories_category" "product_category" ON "product_category"."productId"="product"."id"
+LEFT JOIN "category" "category" ON "category"."id"="product_category"."categoryId"
+
+WHERE "category"."id" IN (10)
