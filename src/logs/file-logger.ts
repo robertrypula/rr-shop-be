a5a2417de935b363098f4c3422cb8578b0d06868
Robert Rypuła <robert.rypula@gmail.com>
2020-04-26 22:42:29 +0200

    Changes in payu notification loging directory

--- src/logs/file-logger.ts
+++ src/logs/file-logger.ts
@@ -7,5 +7,5 @@ export const fileLogger = (content: string, filenamePrefix = ''): void => {
   const filenamePrefixFormatted: string = filenamePrefix !== '' ? filenamePrefix + '_' : '';
   const filename = `${filenamePrefixFormatted}${getFormattedDate(new Date())}_${getRandomInt(10000, 99999)}.txt`;
 
-  fileSave(join(__dirname, filename), content);
+  fileSave(join(__dirname, 'pay-u', filename), content);
 };
