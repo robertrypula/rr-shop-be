f542a1ba97d292f2ceee6c05f295f7f07ef32a2b
Robert Rypuła <robert.rypula@gmail.com>
2020-05-10 15:58:27 +0200

    Moves admin product logic to admin services and repositories

--- src/admin/services/product/admin-product-repository.service.ts
+++ src/admin/services/product/admin-product-repository.service.ts
@@ -5,7 +5,47 @@ import { Product } from '../../../entity/product';
 export class AdminProductRepositoryService {
   public constructor(protected repository: Repository<Product> = getRepository(Product)) {}
 
-  // ---------------------------------------------------------------------------
+  public async getAdminProduct(id: number): Promise<Product> {
+    const selectQueryBuilder: SelectQueryBuilder<Product> = this.repository
+      .createQueryBuilder('product')
+      .select([
+        'product',
+        'orderItems',
+        ...['order.number', 'order.status'],
+        ...['orderItemsSupplies.bestBefore'],
+        'productSupplies',
+        'productSuppliesOrderItem.priceUnitSelling',
+        ...['productSuppliesOrderItemOrder.number', 'productSuppliesOrderItemOrder.status'],
+        'distributor',
+        'manufacturer'
+      ])
+      .leftJoin('product.orderItems', 'orderItems')
+      .leftJoin('orderItems.order', 'order')
+      .leftJoin('orderItems.supplies', 'orderItemsSupplies')
+      .leftJoin('product.supplies', 'productSupplies')
+      .leftJoin('productSupplies.orderItem', 'productSuppliesOrderItem')
+      .leftJoin('productSuppliesOrderItem.order', 'productSuppliesOrderItemOrder')
+      .leftJoin('product.distributor', 'distributor')
+      .leftJoin('product.manufacturer', 'manufacturer')
+      .where('product.id = :id', { id });
+
+    return await selectQueryBuilder.getOne();
+  }
+
+  public async getAdminProducts(): Promise<Product[]> {
+    const selectQueryBuilder: SelectQueryBuilder<Product> = this.repository
+      .createQueryBuilder('product')
+      .select([
+        ...['id', 'externalId', 'name', 'nameCashRegister', 'priceUnit', 'isHidden', 'type'].map(c => `product.${c}`),
+        'manufacturer.name'
+      ])
+      // .leftJoin('product.distributor', 'distributor')
+      .leftJoin('product.manufacturer', 'manufacturer')
+      // .where('product.type = :type', { type: Type.Product })
+      .orderBy('product.externalId', 'ASC');
+
+    return await selectQueryBuilder.getMany();
+  }
 
   public async getAdminProductWithNoRelations(id: number): Promise<Product> {
     const selectQueryBuilder: SelectQueryBuilder<Product> = this.repository
--- src/admin/services/product/admin-product.service.ts
+++ src/admin/services/product/admin-product.service.ts
@@ -1,24 +1,43 @@
 import { Product } from '../../../entity/product';
+import { ProductService } from '../../../services/product/product.service';
 import { getSlugFromPolishString } from '../../../utils/name.utils';
 import { removeMultipleWhitespaceCharacters } from '../../../utils/transformation.utils';
-import { AdminProductPatch } from '../../rest-api/product.models';
+import { AdminProductWriteRequestBody } from '../../rest-api/product.models';
 import { AdminProductRepositoryService } from './admin-product-repository.service';
 
 export class AdminProductService {
   public constructor(
-    protected adminProductRepositoryService: AdminProductRepositoryService = new AdminProductRepositoryService()
+    protected adminProductRepositoryService: AdminProductRepositoryService = new AdminProductRepositoryService(),
+    protected productService: ProductService = new ProductService()
   ) {}
 
-  public async patch(id: number, body: AdminProductPatch): Promise<void> {
+  public async getAdminProduct(id: number): Promise<Product> {
+    return await this.adminProductRepositoryService.getAdminProduct(id);
+  }
+
+  public async getAdminProducts(): Promise<Product[]> {
+    const products: Product[] = await this.adminProductRepositoryService.getAdminProducts();
+
+    await this.productService.attachOrderItemsStubs(products, null);
+    await this.productService.attachSuppliesStubs(products, null);
+
+    return this.productService.triggerCalculations(products, false);
+  }
+
+  public async patch(id: number, body: AdminProductWriteRequestBody): Promise<void> {
     const product: Product = await this.adminProductRepositoryService.getAdminProductWithNoRelations(id);
 
+    await this.fill(product, body);
+
+    await this.adminProductRepositoryService.save(product);
+  }
+
+  public async fill(product: Product, body: AdminProductWriteRequestBody): Promise<void> {
     product.name = removeMultipleWhitespaceCharacters(body.name).trim();
     product.slug = getSlugFromPolishString(product.name);
     product.description = body.description ? body.description.replace(/\r/g, '').trim() : null;
     product.priceUnit = body.priceUnit;
     product.notes = body.notes;
     product.isHidden = body.isHidden;
-
-    await this.adminProductRepositoryService.save(product);
   }
 }
