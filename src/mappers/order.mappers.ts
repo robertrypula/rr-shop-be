9b19d9edac16deab14f84d21ec8d3975badeef12
Robert Rypuła <robert.rypula@gmail.com>
2020-05-31 21:14:48 +0200

    Fixes create order bug (missing address, zipCode and city)

--- src/mappers/order.mappers.ts
+++ src/mappers/order.mappers.ts
@@ -8,9 +8,9 @@ export const fromOrderCreateRequestDto = (dto: OrderCreateRequestDto): Order => 
   order.phone = dto.phone;
   order.name = dto.name;
   order.surname = dto.surname;
-  order.address = dto.address;
-  order.zipCode = dto.zipCode;
-  order.city = dto.city;
+  order.address = dto.address ? dto.address : '';
+  order.zipCode = dto.zipCode ? dto.zipCode : '';
+  order.city = dto.city ? dto.city : '';
   order.comments = dto.comments ? dto.comments : null;
   order.parcelLocker = dto.parcelLocker ? dto.parcelLocker : null;
 
